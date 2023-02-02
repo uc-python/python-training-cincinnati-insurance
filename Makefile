0014d3da68d86c6b82c1eaf582de9521cb3b3dc8
Gus Powers <guspowers0@gmail.com>
2023-02-02 14:22:51 -0500

    map html updates to individual notebooks

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
-slides: slides/.finished
-slides/.finished: $(wildcard notebooks/**/*)
-	bash scripts/generate_slides.sh
-	touch slides/.finished
+slides: html
+
+NBFILES=$(wildcard notebooks/*-*.ipynb)
+HTMLFILES=$(patsubst notebooks/%.ipynb,slides/%.slides.html,$(NBFILES))
+
+html: $(HTMLFILES)
+
+$(HTMLFILES): slides/%.slides.html: notebooks/%.ipynb
+	bash scripts/generate_slides.sh $<
 
 clean:
 	rm -rf slides/
--- scripts/generate_slides.sh
+++ scripts/generate_slides.sh
@@ -13,6 +13,9 @@ cd notebooks
 mkdir -p ../slides/images/
 cp -a ./images/* ../slides/images/
 # Match all notebook files with content.
-for file in *-*.ipynb; do
-    jupyter nbconvert --to slides $file --output-dir=../slides
-done
+#for file in *-*.ipynb; do
+#    jupyter nbconvert --to slides $file --output-dir=../slides
+#done
+NB_PATH="$1"
+REL_NB=${NB_PATH//notebooks\//}
+jupyter nbconvert --to slides $REL_NB --output-dir=../slides
